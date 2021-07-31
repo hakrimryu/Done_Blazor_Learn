6b54c6975cc5d01119ca93dc91fce92424f48319
hakrimryu <hakrimryu@gmail.com>
2021-07-31 20:37:48 +0900

    CountをState管理

---
+++ BlazorApp/Data/CounterState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class CounterState
+    {
+        int _count = 0;
+
+        public Action OnStateChanged;
+
+        public int Count 
+        {
+            get 
+            {
+                return this._count;
+            }
+            set 
+            {
+                this._count = value;
+                this.Refresh();
+            }
+        }
+
+        void Refresh()
+        {
+            this.OnStateChanged?.Invoke();
+        }
+    }
+}
